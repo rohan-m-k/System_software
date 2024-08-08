241204794784053f6094c16ce64929e39965da2b
Rohan katkar <160541304+rohan-m-k@users.noreply.github.com>
2024-08-09 00:03:20 +0530

    keyword.c

--- keyword.c
+++ keyword.c
@@ -1,69 +1,51 @@
-#include <stdio.h>
-#include <ctype.h>
-#include <string.h>
-
-#define MAX_KEYWORDS 32
-#define MAX_BUFFER_SIZE 256
-#define MAX_SOURCE_SIZE 1024
-
-const char *keywords[MAX_KEYWORDS] = {
+#include<stdio.h>
+#include<string.h>
+#include<ctype.h>
+#include<stdlib.h>
+int iskeyword(char buffer[])
+{
+    char k[32][10] = {
     "auto", "break", "case", "char", "const", "continue", "default", "do", 
     "double", "else", "enum", "extern", "float", "for", "goto", "if", 
     "int", "long", "register", "return", "short", "signed", "sizeof", 
     "static", "struct", "switch", "typedef", "union", "unsigned", 
     "void", "volatile", "while"
 };
-
-int isKeyword(const char *str) {
-    for (int i = 0; i < MAX_KEYWORDS; i++) {
-        if (strcmp(str, keywords[i]) == 0) {
-            return 1;
-        }
-    }
-    return 0;
+	int i,flag=0;
+	for(i=0;i<32;i++){
+		if(strcmp(k[i],buffer)==0){
+			flag=1;
+			break;
+		}
+	}
+		return flag;
 }
-
-void lexicalAnalysis(const char *sourceCode) {
-    char buffer[MAX_BUFFER_SIZE];
-    int i = 0, j = 0;
-
-    while (sourceCode[i] != '\0') {
-        if (isalnum(sourceCode[i])) {
-            buffer[j++] = sourceCode[i];
-        } else if ((sourceCode[i] == ' ' || sourceCode[i] == '\n' || sourceCode[i] == '\t' || 
-                    sourceCode[i] == ',' || sourceCode[i] == ';' || sourceCode[i] == '(' || 
-                    sourceCode[i] == ')') && j > 0) {
-            buffer[j] = '\0';
-            j = 0;
-            
-            if (isKeyword(buffer)) {
-               printf("Keyword: %s\n", buffer);
-            }
-        }
-        i++;
-    }
-}
-
-
-int main() {
-    char sourceCode[MAX_SOURCE_SIZE];
-    char filename[MAX_BUFFER_SIZE];
-
-    printf("Enter the filename of the source code: ");
-    scanf("%s", filename);
-
-    FILE *file = fopen(filename, "r");
-    if (file == NULL) {
-        printf("Error: Could not open file %s\n", filename);
-        return 1;
-    }
-
-    printf("Source Code:\n");
-    while (fgets(sourceCode, MAX_SOURCE_SIZE, file)) {
-        printf("%s", sourceCode);
-        lexicalAnalysis(sourceCode);
-    }
-
-    fclose(file);
-    return 0;
+int main()
+{
+	int i=0;
+    //int ch;
+	char buffer[15];
+	FILE*fp;
+	fp=fopen("loader.c","r");
+	if(fp==NULL){
+		printf("while opening error");
+		exit(0);
+	}
+	int ch;
+	while((ch=fgetc(fp))!=EOF){
+		if(isalpha(ch)){
+			buffer[i++]=ch;
+		}
+		else if(i>0){
+			buffer[i]='\0';
+			i=0;
+			if(iskeyword(buffer)==1)
+			{
+				printf("%s is keyword\n",buffer);
+			}
+			buffer[0]='\0';
+		}
+	}
+	fclose(fp);
+	return 0;
 }
