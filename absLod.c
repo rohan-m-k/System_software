af048ac42cfe0db3b52dffe357e01637a380bc0c
Rohan katkar <160541304+rohan-m-k@users.noreply.github.com>
2024-08-09 00:05:46 +0530

    absoulte_loader.c

--- absLod.c
+++ absLod.c
@@ -1,114 +1,114 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-
-int main(){
-    FILE *ip,*op;
-    char str[300], len[10];
-    int i = 0, j, start, st, byte_count;
-    ip = fopen("objcode.txt", "r");
-    op = fopen("Absout.txt", "w");
-    fscanf(ip, "%s\n", str);
-    fprintf(op, "MEMORY ADDRESS\n");
-
-    while (fscanf(ip, "%s\n", str) >= 1) {
-        if (str[0] == 'E') break;
-        for (int k = 2; k < 8; k++) {
-            len[k - 2] = str[k];
-        }
-        len[6] = '\0';
-        start = (int)strtoul(len, NULL, 16);
-
-        i = 12;
-        while (str[i] != '\0') {
-            for (byte_count = 0; byte_count < 16 && str[i] != '\0'; byte_count++, start++) {
-                fprintf(op, "%06x ", start);
-                i += 2;
-            }
-            fprintf(op, "\n");
-            start -= byte_count;  // Reset start to the beginning of the line
-            i -= (byte_count * 2);  // Reset i to the beginning of the line
-            for (byte_count = 0; byte_count < 16 && str[i] != '\0'; byte_count++, start++) {
-                for (j = 0; j < 2; j++) {
-                    fprintf(op, "%c", str[i + j]);
-                }
-                fprintf(op, "\t  ");
-                i += 2;
-            }
-            fprintf(op, "\n\n");
-        }
-    }
-
-    printf("Output is in Absout.txt\n");
-    fclose(ip);
-    fclose(op);
-    return 0;
+#include <stdio.h>
+#include <string.h>
+char input[10], label[10], ch1, ch2;
+int addr, w = 0, start, ptaddr, l, length = 0, end, count = 0, k, taddr, address, i = 0;
+FILE *fp1, *fp2;
+void check();
+void main()
+{
+	fp1 = fopen("abc.txt", "r");
+	fp2 = fopen("Absout.txt", "w");
+	fscanf(fp1, "%s", input);
+	printf("\n\nABSOLUTE LOADER\n");
+	fprintf(fp2, "\n-------------------------------------------------------\n");
+	fprintf(fp2, "MEMORY ADDRESS\t\t\tCONTENTS");
+	fprintf(fp2, "\n-------------------------------------------------------\n");
+	while (strcmp(input, "E") != 0)
+	{
+		if (strcmp(input, "H") == 0)
+		{
+			fscanf(fp1, "%s %x %x %s", label, &start, &end, input);
+			address = start;
+		}
+		else if (strcmp(input, "T") == 0)
+		{
+			l = length;
+			ptaddr = addr;
+			fscanf(fp1, "%x %x %s", &taddr, &length, input);
+			addr = taddr;
+			if (w == 0)
+			{
+				ptaddr = address;
+				w = 1;
+			}
+			for (k = 0; k < (taddr - (ptaddr + l)); k++)
+			{
+				address = address + 1;
+				fprintf(fp2, "xx");
+				count++;
+				if (count == 4)
+				{
+					fprintf(fp2, "  ");
+					i++;
+					if (i == 4)
+					{
+						fprintf(fp2, "\n\n%x\t\t", address);
+						i = 0;
+					}
+					count = 0;
+				}
+			}
+			if (taddr == start)
+				fprintf(fp2, "\n\n%x\t\t", taddr);
+			fprintf(fp2, "%c%c", input[0], input[1]);
+			check();
+			fprintf(fp2, "%c%c", input[2], input[3]);
+			check();
+			fprintf(fp2, "%c%c", input[4], input[5]);
+			check();
+			fscanf(fp1, "%s", input);
+		}
+		else
+		{
+			fprintf(fp2, "%c%c", input[0], input[1]);
+			check();
+			fprintf(fp2, "%c%c", input[2], input[3]);
+			check();
+			fprintf(fp2, "%c%c", input[4], input[5]);
+			check();
+			fscanf(fp1, "%s", input);
+		}
+	}
+	fprintf(fp2, "\n-------------------------------------------------------\n");
+	fclose(fp1);
+	fclose(fp2);
+	printf("\n\n The contents of output file:\n");
+	fp2 = fopen("Absout.txt", "r");
+	ch2 = fgetc(fp2);
+	while (ch2 != EOF)
+	{
+		printf("%c", ch2);
+		ch2 = fgetc(fp2);
+	}
+	fclose(fp2);
+}
+void check()
+{
+	count++;
+	address++;
+	taddr = taddr + 1;
+	if (count == 4)
+	{
+		fprintf(fp2, "  ");
+		i++;
+		if (i == 4)
+		{
+			fprintf(fp2, "\n\n%x\t\t", taddr);
+			i = 0;
+		}
+		count = 0;
+	}
 }
 
+/* 
 
+input.txt
+---------
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// #include<stdio.h>
-// #include<stdlib.h>
-// #include<string.h>
-// int main(){
-//     FILE *ip,*op;
-//     char str[300],len[10];
-//     int i=0,j,start,temp,st;
-//     ip=fopen("output.txt","r");
-//     op=fopen("Absout.txt","w");
-//     fscanf(ip,"%s\n",str);
-//     fprintf(op,"MEMORY ADDRESS\t\tCONTENTS\n");
-//     if(str[i]=='H'){
-//         for(;str[i]!='^';i++);
-//         i++;
-//         for(;str[i]!='^';i++);
-//         i++;
-//         for(j=0;j<6;j++)
-//         len[j]=str[i+j];
-//         len[6]='\0';
-//     }
-//     st=(int)strtoul(len,NULL,16);
-//     while(fscanf(ip,"%s\n",str)>=1){
-//         if(str[0]=='E') break;
-//         for(int k=2;k<8;k++){
-//             len[k-2]=str[k];
-//         }
-//         len[6]='\0';
-//         //start=(int)strtoul(len, NULL, 16)-st;
-//         start=(int)strtoul(len, NULL, 16);
-//         for(i=12;str[i]!='\0';){
-//             fprintf(op,"\n%06x\t\t\t\t    ",start);
-//             for(j=0;j<2;j++){
-//                 fprintf(op,"%c",str[i+j]);
-//             }
-//             i+=2;
-//             start+=1;
-//         }
-//     }
-//     printf("Output is in Absout.txt\n");
-//     return 0;
-// }
+H COPY 001000 00107A
+T 001000 1E 141033 482039 001036 281030 301015 482061 3C1003 00102A 0C1039 00102D
+T 00101E 15 0C1036 482061 081033 4C0000 454F46 000003 000000
+T 001047 1E 041030 001030 E0205D 30203F D8205D 281030 302057 549039 2C205E 38203F
+T 001077 1C 101036 4C0000 000000 001000 041030 E02079 302064 509039 DC2079 2C1036
+E 001000
+*/
